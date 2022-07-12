03cbf715007058603cfb116e279b35b58af65241
Noah Da Silva <noah@noahdasilva.com>
2022-07-11 20:32:02 -0400

    Changed user model __str__

--- noahdasilva_com/account/models.py
+++ noahdasilva_com/account/models.py
@@ -47,5 +47,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('users')
 
     def __str__(self):
-        return self.email
+        if self.full_name:
+            return self.full_name
+        else:
+            return self.username
 
