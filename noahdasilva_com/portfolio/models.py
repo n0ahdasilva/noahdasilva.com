60489bee7b800cf068b8ab4451ad0d865587484d
Noah Da Silva <noah@noahdasilva.com>
2022-07-13 00:14:21 -0400

    Created project model for porfolio app

--- noahdasilva_com/portfolio/models.py
+++ noahdasilva_com/portfolio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(max_length=128, unique=True, nuxll=True, blank=True)
+    description = models.TextField(max_length=512)
+    category = models.CharField(max_length=64)
+    url = models.URLField(max_length=128, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='portfolio_images/')
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Project, self).save(*args, **kwargs)
